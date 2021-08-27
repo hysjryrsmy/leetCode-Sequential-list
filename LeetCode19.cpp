ebceae122f727b808603635f47d137569efb2d3e
yiyi <1484491854a@gmail.com>
2021-08-27 15:07:31 +0900

    LeetCode19

---
+++ LeetCode19.cpp
@@ -0,0 +1,74 @@
+﻿
+//19. 删除链表的倒数第 N 个结点
+
+
+//实例1
+//输入：head = [1, 2, 3, 4, 5], n = 2
+//输出：[1, 2, 3, 5]
+//示例 2：
+//输入：head = [1], n = 1
+//输出：[]
+//示例 3：
+//输入：head = [1, 2], n = 1
+//输出：[1]
+
+//提示：
+//链表中结点的数目为 sz
+//1 <= sz <= 30
+//0 <= Node.val <= 100
+//1 <= n <= sz
+
+/*
+C++
+class Solution {
+public:
+    int getLength(ListNode* head) {
+        int length = 0;
+        while (head) {
+            ++length;
+            head = head->next;
+        }
+        return length;
+    }
+
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode(0, head);
+        int length = getLength(head);
+        ListNode* cur = dummy;
+        for (int i = 1; i < length - n + 1; ++i) {
+            cur = cur->next;
+        }
+        cur->next = cur->next->next;
+        ListNode* ans = dummy->next;
+        delete dummy;
+        return ans;
+    }
+};
+*/
+
+
+/*
+C
+int getLength(struct ListNode* head) {
+    int length = 0;
+    while (head) {
+        ++length;
+        head = head->next;
+    }
+    return length;
+}
+
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
+    struct ListNode* dummy = malloc(sizeof(struct ListNode));
+    dummy->val = 0, dummy->next = head;
+    int length = getLength(head);
+    struct ListNode* cur = dummy;
+    for (int i = 1; i < length - n + 1; ++i) {
+        cur = cur->next;
+    }
+    cur->next = cur->next->next;
+    struct ListNode* ans = dummy->next;
+    free(dummy);
+    return ans;
+}
+*/
