3cdedca2cbb4cb2527c643d7eb215ca25c0be18b
hysjryrsmy <78720177+hysjryrsmy@users.noreply.github.com>
2021-09-09 16:25:09 +0900

    Add files via upload

---
+++ 42.cpp
@@ -0,0 +1,111 @@
+﻿
+//42. 接雨水
+//
+//给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+//
+//示例 1：
+//输入：height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+//输出：6
+//解释：上面是由数组[0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
+//
+//示例 2：
+//输入：height = [4, 2, 0, 3, 2, 5]
+//输出：9
+//
+//提示：
+//n == height.length
+//0 <= n <= 3 * 104
+//0 <= height[i] <= 105
+
+
+
+//方法 1：暴力
+//C++
+int trap(vector<int>& height)
+{
+    int ans = 0;
+    int size = height.size();
+    for (int i = 1; i < size - 1; i++) {
+        int max_left = 0, max_right = 0;
+        for (int j = i; j >= 0; j--) { //Search the left part for max bar size
+            max_left = max(max_left, height[j]);
+        }
+        for (int j = i; j < size; j++) { //Search the right part for max bar size
+            max_right = max(max_right, height[j]);
+        }
+        ans += min(max_left, max_right) - height[i];
+    }
+    return ans;
+}
+
+
+
+//方法 2：动态编程
+//C++
+int trap(vector<int>& height)
+{
+    if (height == null)
+        return 0;
+    int ans = 0;
+    int size = height.size();
+    vector<int> left_max(size), right_max(size);
+    left_max[0] = height[0];
+    for (int i = 1; i < size; i++) {
+        left_max[i] = max(height[i], left_max[i - 1]);
+    }
+    right_max[size - 1] = height[size - 1];
+    for (int i = size - 2; i >= 0; i--) {
+        right_max[i] = max(height[i], right_max[i + 1]);
+    }
+    for (int i = 1; i < size - 1; i++) {
+        ans += min(left_max[i], right_max[i]) - height[i];
+    }
+    return ans;
+}
+
+
+
+
+//方法 3：栈的应用
+//C++
+int trap(vector<int>& height)
+{
+    int ans = 0, current = 0;
+    stack<int> st;
+    while (current < height.size()) {
+        while (!st.empty() && height[current] > height[st.top()]) {
+            int top = st.top();
+            st.pop();
+            if (st.empty())
+                break;
+            int distance = current - st.top() - 1;
+            int bounded_height = min(height[current], height[st.top()]) - height[top];
+            ans += distance * bounded_height;
+        }
+        st.push(current++);
+    }
+    return ans;
+}
+
+
+
+
+//方法 4：使用双指针
+//C++
+int trap(vector<int>& height)
+{
+    int left = 0, right = height.size() - 1;
+    int ans = 0;
+    int left_max = 0, right_max = 0;
+    while (left < right) {
+        if (height[left] < height[right]) {
+            height[left] >= left_max ? (left_max = height[left]) : ans += (left_max - height[left]);
+            ++left;
+        }
+        else {
+            height[right] >= right_max ? (right_max = height[right]) : ans += (right_max - height[right]);
+            --right;
+        }
+    }
+    return ans;
+}
